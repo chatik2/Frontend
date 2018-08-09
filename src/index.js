00c8a2f2cc76c5495747c72f1afbad72b25516c4
Elwerco <alexsteev2002@gmail.com>
2018-08-09 13:25:07 +0300

    3-4 final #3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 import App from './App';
 import reducer from './reducers';
 
